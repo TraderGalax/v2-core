3143dab2a4ef86026f2989ef7115553376b5c2d0
TraderGalax <30950645+TraderGalax@users.noreply.github.com>
2022-06-23 11:48:53 +0800

    Update UniswapV2Factory.sol

--- contracts/UniswapV2Factory.sol
+++ contracts/UniswapV2Factory.sol
@@ -1,49 +1,171 @@
-pragma solidity =0.5.16;
+pragma solidity ^0.8.0;
 
-import './interfaces/IUniswapV2Factory.sol';
-import './UniswapV2Pair.sol';
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
-contract UniswapV2Factory is IUniswapV2Factory {
-    address public feeTo;
-    address public feeToSetter;
+contract LittleMamiV1ERC20 is ERC20Burnable {
+    // EIP712Domain
+    bytes32 public DOMAIN_SEPARATOR;
 
-    mapping(address => mapping(address => address)) public getPair;
-    address[] public allPairs;
-
-    event PairCreated(address indexed token0, address indexed token1, address pair, uint);
-
-    constructor(address _feeToSetter) public {
-        feeToSetter = _feeToSetter;
-    }
-
-    function allPairsLength() external view returns (uint) {
-        return allPairs.length;
-    }
-
-    function createPair(address tokenA, address tokenB) external returns (address pair) {
-        require(tokenA != tokenB, 'UniswapV2: IDENTICAL_ADDRESSES');
-        (address token0, address token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
-        require(token0 != address(0), 'UniswapV2: ZERO_ADDRESS');
-        require(getPair[token0][token1] == address(0), 'UniswapV2: PAIR_EXISTS'); // single check is sufficient
-        bytes memory bytecode = type(UniswapV2Pair).creationCode;
-        bytes32 salt = keccak256(abi.encodePacked(token0, token1));
+    constructor() public ERC20("LittleMami V1", "LM-V1") {
+        uint256 chainId;
         assembly {
-            pair := create2(0, add(bytecode, 32), mload(bytecode), salt)
+            chainId := chainid()
         }
-        IUniswapV2Pair(pair).initialize(token0, token1);
-        getPair[token0][token1] = pair;
-        getPair[token1][token0] = pair; // populate mapping in the reverse direction
-        allPairs.push(pair);
-        emit PairCreated(token0, token1, pair, allPairs.length);
+        DOMAIN_SEPARATOR = keccak256(
+            abi.encode(
+                keccak256(
+                    "EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"
+                ),
+                keccak256(bytes(name())),
+                keccak256(bytes("1")),
+                chainId,
+                address(this)
+            )
+        );
+    }
+}
+
+contract LittleMamiV1IFO is Ownable, ReentrancyGuard, LittleMamiV1ERC20 {
+    event Invest(address indexed investor, uint256 amount);
+
+    constructor() {
+        factory = msg.sender;
     }
 
-    function setFeeTo(address _feeTo) external {
-        require(msg.sender == feeToSetter, 'UniswapV2: FORBIDDEN');
-        feeTo = _feeTo;
+    address public factory;
+    string public ifoName;
+    string public logo;
+    string public description;
+    uint256 public hardcap;
+    uint256 public startTime;
+    uint256 public endTime;
+
+    uint256 public currentAmount;
+
+    // called once by the factory at time of deployment
+    function initialize(
+        string memory _name,
+        string memory _logo,
+        string memory _description,
+        uint256 _hardcap,
+        uint256 _startTime,
+        uint256 _endTime,
+        address _owner
+    ) external onlyOwner {
+        ifoName = _name;
+        logo = _logo;
+        description = _description;
+        hardcap = _hardcap;
+        startTime = _startTime;
+        endTime = _endTime;
+        transferOwnership(_owner);
     }
 
-    function setFeeToSetter(address _feeToSetter) external {
-        require(msg.sender == feeToSetter, 'UniswapV2: FORBIDDEN');
-        feeToSetter = _feeToSetter;
+    function invest() external payable nonReentrant {
+        uint256 value = msg.value;
+        require(
+            currentAmount + value <= hardcap,
+            "LittleMami V1 Protocol : CurrentAmount must be less than or equal to hard cap"
+        );
+        require(
+            block.timestamp >= startTime,
+            "LittleMami V1 Protocol : IFO has not started"
+        );
+        require(
+            block.timestamp <= endTime,
+            "LittleMami V1 Protocol : IFO is over"
+        );
+        currentAmount += value;
+        _mint(msg.sender, value);
+        emit Invest(msg.sender, value);
+    }
+
+    function edit(
+        string calldata _name,
+        string calldata _logo,
+        string calldata _description,
+        uint256 _hardcap,
+        uint256 _startTime,
+        uint256 _endTime
+    ) external onlyOwner {
+        require(
+            hardcap >= currentAmount,
+            "LittleMami V1 Protocol : CurrentAmount Must be less than or equal to hard cap"
+        );
+        ifoName = _name;
+        logo = _logo;
+        description = _description;
+        hardcap = _hardcap;
+        startTime = _startTime;
+        endTime = _endTime;
+    }
+
+    function withdraw() external onlyOwner {
+        payable(msg.sender).transfer(address(this).balance);
+    }
+}
+
+contract LittleMamiV1Factory {
+    using Strings for uint256;
+
+    event CreateIFO(address indexed ifo, uint256 id);
+
+    uint256 id;
+
+    function createIFO(
+        string calldata _name,
+        string calldata _logo,
+        string calldata _description,
+        uint256 _hardcap,
+        uint256 _startTime,
+        uint256 _endTime
+    ) external payable {
+        id++;
+        bytes32 salt = keccak256(abi.encodePacked(id.toString()));
+
+        // create ifo contract | create2
+        LittleMamiV1IFO ifo = new LittleMamiV1IFO{salt: salt}();
+
+        ifo.initialize(
+            _name,
+            _logo,
+            _description,
+            _hardcap,
+            _startTime,
+            _endTime,
+            msg.sender
+        );
+
+        emit CreateIFO(address(ifo), id);
+    }
+
+    function totalIFO() external view returns (uint256) {
+        return id;
+    }
+
+    function getIFOAddress(uint256 _id) external view returns (address) {
+        require(
+            _id <= id && _id != 0,
+            "LittleMami V1 Protocol : Not found this id"
+        );
+        bytes32 salt = keccak256(abi.encodePacked(_id.toString()));
+        return
+            address(
+                uint160(
+                    uint256(
+                        keccak256(
+                            abi.encodePacked(
+                                bytes1(0xff),
+                                address(this),
+                                salt,
+                                keccak256(type(LittleMamiV1IFO).creationCode)
+                            )
+                        )
+                    )
+                )
+            );
     }
 }
